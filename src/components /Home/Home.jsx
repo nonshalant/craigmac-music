d124e9aaee440bfdaa3047ec57cefb12ab50db7a
nonshalant <shamir.gibbons@gmail.com>
2023-11-28 20:19:00 -0500

    development begin

--- src/components /Home/Home.jsx
+++ src/components /Home/Home.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import './home.css'
 import { craigMacContent } from '../content'
+import { MediaQuery, useMediaQuery } from 'react-responsive';
 import Footer from '../Footer/Footer';
 
 const Home = () => {
     const video = craigMacContent.homePageGif.url;
+    const isMobile = useMediaQuery({ maxWidth: 767 });
 
     return (
         <div className='home-gif'>
-            <video width='100000' muted autoPlay loop>
-                <source src={video} type="video/mp4"></source>
-            </video>
+            {
+                isMobile &&
+                    <video className='video' width='500vw' height='700vh' muted autoPlay loop>
+                        <source src={video} type="video/mp4"></source>
+                    </video> 
+            }   
+             {/* <video className='video' width='100000' muted autoPlay loop>
+                        <source src={video} type="video/mp4"></source>
+                    </video>  */}
             <Footer />
         </div>
     )
