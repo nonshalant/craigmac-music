2d3061ff26ceb13090ae4abfb1b8ab970845615f
nonshalant <shamir.gibbons@gmail.com>
2023-11-28 23:54:43 -0500

    final changes

--- src/components /Footer/Footer.jsx
+++ src/components /Footer/Footer.jsx
@@ -12,14 +12,14 @@ const Footer = () => {
     <footer className='footer'>
         <div className="footer-container">
             <a href='https://www.everlastingapps.com' target='_blank'>Website by <span>@Ever Lasting Apps</span></a>
-            <div className="footer-links">
+            {/* <div className="footer-links">
                 <FaEnvelope />
                 <a href="https://music.apple.com/us/artist/craigmac/1507596398" target='_blank' ><FaMusic/></a>
                 <a href="https://open.spotify.com/artist/6PFTqKrnFKJoZmN3c9MQy2?si=sq5zEmLHS8m38DZa3WLowQ" target='_blank'><FaSpotify /></a>
                 <a href="https://www.youtube.com/@craigmac7608" target='_blank'><FaYoutube /></a>
                 <a href="https://www.tiktok.com/@officialcraigmac?_t=8hiadcpaZlX&_r=1" target='_blank'><FaTiktok /></a>
                 <a href="https://www.instagram.com/craigmacofficial__/" target='_blank'><FaInstagramSquare /></a>
-            </div>
+            </div> */}
         </div>
     </footer>
   )
