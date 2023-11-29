78a966c49cebc688e1da93546bf3886c1cf382c4
nonshalant <shamir.gibbons@gmail.com>
2023-11-28 21:59:06 -0500

    fixed header and toolbar at bottom

--- src/components /Header/Header.jsx
+++ src/components /Header/Header.jsx
@@ -11,13 +11,21 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import { Image } from 'cloudinary-react';
 import { craigMacContent } from '../content';
 import { useMediaQuery } from 'react-responsive';
-
+import { FaWindowClose } from "react-icons/fa";
 
 const Header = () => {
     const image = craigMacContent.header.url;
     const cloudName='dyzydm9nl';
     const isMobile = useMediaQuery({maxWidth: '767px'});
     const [navOpen, setNavOpen] = useState(false);
+
+    const openNav = () =>{ 
+        setNavOpen(true);
+    };
+
+    const closeNav = () =>{
+        setNavOpen(false)
+    };
 
   return (
     <header>
@@ -26,36 +34,45 @@ const Header = () => {
         </div> */}
         {
             isMobile ?
-            <nav className='nav-bar'>
-                {/* <ul className='nav-links'>
-                    <Link to='/music'> 
-                        <li>Music</li> 
-                    </Link>
-                    <Link to='/videos'>
-                        <li>Videos</li>
-                    </Link>
-                    <Link to='/merch'> 
-                        <li>Merch</li>
-                    </Link>
-                    <Link to='news'>
-                        <li>News</li>
-                    </Link>
-                </ul> */}
-                <div className="logo">
-                    {/* <Image className="logoBg" cloudName="dyzydm9nl" publicId="craig%20mac/IMG_6667_d0uie2.jpg" ></Image> */}
-                    <Link to='/'><h1>Craig Mac</h1></Link>
-                    <GiHamburgerMenu size={30}/>
+            <>
+                <div className="mobile-nav-top">
+                    {
+                        navOpen &&
+                        <ul className='nav-links'>
+                            <>
+                                <Link to='/music'> 
+                                    <li onClick={closeNav}>Music</li> 
+                                </Link>
+                                <Link to='/videos'>
+                                    <li onClick={closeNav}>Videos</li>
+                                </Link>
+                                <Link to='/merch'> 
+                                    <li onClick={closeNav}>Merch</li>
+                                </Link>
+                                <Link to='news'>
+                                    <li onClick={closeNav}>News</li>
+                                </Link>
+                            </>
+                            <FaWindowClose onClick={closeNav} />
+                        </ul>
+                    }
+                    <div className="logo">
+                        {/* <Image className="logoBg" cloudName="dyzydm9nl" publicId="craig%20mac/IMG_6667_d0uie2.jpg" ></Image> */}
+                        <Link to='/'><h1>Craig Mac</h1></Link>
+                        <GiHamburgerMenu onClick={openNav} size={30}/>
+                    </div>
                 </div>
-
-                <ul className='media'>
-                    <a href="https://music.apple.com/us/artist/craigmac/1507596398" target='_blank' ><FaMusic/></a>
-                    <a href="https://open.spotify.com/artist/6PFTqKrnFKJoZmN3c9MQy2?si=sq5zEmLHS8m38DZa3WLowQ" target='_blank'><FaSpotify /></a>
-                    <a href="https://www.youtube.com/@craigmac7608" target='_blank'><FaYoutube /></a>
-                    <a href="https://www.tiktok.com/@officialcraigmac?_t=8hiadcpaZlX&_r=1" target='_blank'><FaTiktok /></a>
-                    <a href="https://www.instagram.com/craigmacofficial__/" target='_blank'><FaInstagramSquare /></a>
-                    <FaEnvelope />
-                </ul>
-            </nav>
+                <nav className='nav-bar'>
+                    <ul className='media'>
+                        <a href="https://music.apple.com/us/artist/craigmac/1507596398" target='_blank' ><FaMusic/></a>
+                        <a href="https://open.spotify.com/artist/6PFTqKrnFKJoZmN3c9MQy2?si=sq5zEmLHS8m38DZa3WLowQ" target='_blank'><FaSpotify /></a>
+                        <a href="https://www.youtube.com/@craigmac7608" target='_blank'><FaYoutube /></a>
+                        <a href="https://www.tiktok.com/@officialcraigmac?_t=8hiadcpaZlX&_r=1" target='_blank'><FaTiktok /></a>
+                        <a href="https://www.instagram.com/craigmacofficial__/" target='_blank'><FaInstagramSquare /></a>
+                        <FaEnvelope />
+                    </ul>
+                </nav>
+            </>
         :
         <nav className='nav-bar'>
             <ul className='nav-links'>
